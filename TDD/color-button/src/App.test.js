f60a45eec07c6bbe8d9387076a9229787aa2432d
sooyeon.jang <tn7278@naver.com>
2023-10-02 02:20:30 +0900

    TDD

---
+++ TDD/color-button/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+
+  const button = screen.getByRole('button',{name:'change to blue!'});
+
+  fireEvent.click(button);
+
+  expect(button).toHaveStyle({backgroundColor:'red'});
+  expect(button.textContent).toBe('change to red!');
+  
+});
