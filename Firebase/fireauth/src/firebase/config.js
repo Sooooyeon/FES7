ae0699041c905538c0325e6ad87d12cd3e333e96
sooyeon.jang <tn7278@naver.com>
2023-10-12 23:53:23 +0900

    firebase 환경설정

---
+++ Firebase/fireauth/src/firebase/config.js
@@ -0,0 +1,25 @@
+// config.js 안에 있는 프로젝트관련 값들은 매우 중요해 노출되지 않도록 환경변수를 설정한다.
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// firestore 초기화
+const appFireStore = getFireStore(app);
+
+// 인증 초기화 
+const appAuth = getAuth();
+
+export{appFireStore, appAuth }
